8f0527cd2e2a1552ed3031b68477c1b343ad5f44
netzgestaltung <t.fellinger@netzgestaltung.at>
2023-01-19 12:06:45 +0100

    Update sandbox-disable-comments-feed.php

--- sandbox-disable-comments-feed/sandbox-disable-comments-feed.php
+++ sandbox-disable-comments-feed/sandbox-disable-comments-feed.php
@@ -1,1 +1,60 @@
+<?php
+/**
+ * Plugin Name: Sandbox Disable Comments Feed
+ * Version: 1.0.0
+ * Plugin URI: http://www.dev-themes.com
+ * Description: Restrict public user enumeration in a WordPress multisite
+ * Author: Thomas Fellinger
+ * Author URI: http://www.netzgestaltung.at
+ * License: GPL v2
+ * Copyright 2022  Thomas Fellinger  (email : office@netzgestaltung.at)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Taken from 
+ * - https://wordpress.stackexchange.com/questions/126174/disable-comments-feed-but-not-the-others
+ */
 
+
+/**
+ * Plugin setup hook
+ * 
+ * @since 1.0.0
+ * @added 2022-12-21 Thomas Fellinger
+ */
+add_action('plugins_loaded', 'adv_disable_comments_feed');
+
+function adv_disable_comments_feed(){
+  
+  // Removes Comments Feed pages
+  add_action('do_feed', 'remove_comment_feeds', 9);
+  add_action('do_feed_rdf', 'remove_comment_feeds', 9);
+  add_action('do_feed_rss', 'remove_comment_feeds', 9);
+  add_action('do_feed_rss2', 'remove_comment_feeds', 9);
+  add_action('do_feed_atom', 'remove_comment_feeds', 9);
+
+  // Removes comments feed links from wp head
+  add_filter('feed_links_show_comments_feed', '__return_false'); 
+}
+
+/**
+ * Removes Comments Feed pages and redirects to homepage
+ * 
+ * @since 1.0.0
+ * @added 2022-12-21 Thomas Fellinger
+ */
+function remove_comment_feeds($for_comments){
+  if ( $for_comments ) {
+    wp_safe_redirect('/');
+  }
+}
